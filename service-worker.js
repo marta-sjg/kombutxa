b9749626baa0ba4036ebf5308d24b72ec90656ee
marta-sjg <tufflicakes@gmail.com>
2026-08-17 23:43:27 +0200

    Actualitza l’app instal·lable

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const cacheName = 'diari-kombutxa-v1';
-const localFiles = ['./', './index.html', './styles.css', './app.js', './logo.png', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(localFiles))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+const cacheName = 'diari-kombutxa-v2';
+const localFiles = ['./', './index.html', './styles.css', './app.js', './firebase-sync.js', './logo.png', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(localFiles)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
+
