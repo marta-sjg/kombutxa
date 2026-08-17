60ba440f881289231c7edf807872492a3e44a3be
marta-sjg <tufflicakes@gmail.com>
2026-08-18 01:14:21 +0200

    Actualitza formularis simplificats

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'diari-kombutxa-v11';
+const cacheName = 'diari-kombutxa-v12';
 const localFiles = ['./', './index.html', './styles.css', './app.js', './firebase-sync.js', './logo.png', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(localFiles)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
